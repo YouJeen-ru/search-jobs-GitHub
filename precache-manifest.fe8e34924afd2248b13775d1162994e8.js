fa096f92b4442c7be6e453e0c5419ee2a71c5429
YouJeen-ru <1.rouch@mail.ru>
2020-11-20 15:45:02 +0300

    Updates

---
+++ precache-manifest.fe8e34924afd2248b13775d1162994e8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c0f948f06325102bc28e559443b41bd9",
+    "url": "/GitHub-Jobs-API-React/index.html"
+  },
+  {
+    "revision": "bc0c3275b475ad11a825",
+    "url": "/GitHub-Jobs-API-React/static/js/2.437a368d.chunk.js"
+  },
+  {
+    "revision": "b4ac570288b3bba4760d47e21228c95c",
+    "url": "/GitHub-Jobs-API-React/static/js/2.437a368d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f326975f3c86a8a4acbc",
+    "url": "/GitHub-Jobs-API-React/static/js/main.db07eb70.chunk.js"
+  },
+  {
+    "revision": "f8de871a0dc9cecabf09",
+    "url": "/GitHub-Jobs-API-React/static/js/runtime-main.7c3f6ba2.js"
+  }
+]);
